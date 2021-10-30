55ad04dc27ee3e54af736345a7300ecac50e898a
Patrick <papybp@gmail.com>
2021-10-29 21:21:15 -0500

    29 oct

--- Assignments/Lab2/src/Question9/DnaSequence.java
+++ Assignments/Lab2/src/Question9/DnaSequence.java
@@ -1,4 +1,24 @@
 package Question9;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class DnaSequence {
+    public static void main(String[] args) {
+
+        System.out.println(storeCodons("GATTCGATC"));
+    }
+    public static ArrayList<String> storeCodons(String str){
+        ArrayList codonsList = new ArrayList<String>();
+
+        for (int i = 0; i <str.length() ; i += 3) {
+            if(str.length()< (i+3)) {break;}
+            codonsList.add(str.substring(i, i+3));
+
+        }
+
+        return codonsList;
+
+    }
 }
