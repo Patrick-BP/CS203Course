df94732d4edb523ba34dae3a9ba1a767d37e8d5b
Patrick <papybp@gmail.com>
2021-11-14 01:20:22 -0600

    14 nov

---
+++ inclass/Comparisons/src/NameDescending.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+
+public class NameDescending implements Comparator<Grade>{
+
+	@Override
+	public int compare(Grade o1, Grade o2) {
+		return o1.name.compareTo(o2.name)*-1;
+	}
+
+}
