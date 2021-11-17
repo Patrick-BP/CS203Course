ed365df4057ea434c5f8e41847c8b59df34f6f95
Patrick <papybp@gmail.com>
2021-11-16 19:36:23 -0600

    16 nov

--- Practice/Lesson1-Arrays/src/udemy/Vehicule/Main.java
+++ Practice/Lesson1-Arrays/src/udemy/Vehicule/Main.java
@@ -1,31 +1,9 @@
 package udemy.Vehicule;
 
-import javax.swing.*;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-
-        if (carEntryWindow.tankSizetextField.getText().isEmpty() || carEntryWindow.tankSizetextField.getText().isBlank()) {
-            try {
-                if (Integer.parseInt(carEntryWindow.tankSizetextField.getText()) > 0) {
-                    tanksise = Integer.parseInt(carEntryWindow.tankSizetextField.getText());
-                } else {
-                    JOptionPane.showMessageDialog(null, "Tank size must be great than 0");
-                    return;
-
-                }
-
-            } catch (Exception w) {
-
-
-                JOptionPane.showMessageDialog(carEntryWindow.submitButton, "Tank size must be a numeric value");
-                return;
-            }
-        }else{
-            JOptionPane.showMessageDialog(carEntryWindow.submitButton, "Tank size cannot be empty");
-            return;
-        }
 
 
 
